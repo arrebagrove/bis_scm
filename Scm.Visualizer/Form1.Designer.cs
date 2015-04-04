ce1fdfdfeebb132b3e1ce336daaf034ad4ca01a5
Danny Meier <danny@danflash.com>
2015-04-04 23:54:46 +0200

    Visualizer erstellt für Problem und Lösung von: http://neo.lcc.uma.es/vrp/vrp-instances/multiple-depot-vrp-instances/

---
+++ Scm.Visualizer/Form1.Designer.cs
@@ -0,0 +1,97 @@
+﻿namespace Scm.Visualizer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel = new System.Windows.Forms.Panel();
+            this.buttonLoadInstance = new System.Windows.Forms.Button();
+            this.openFileDialog = new System.Windows.Forms.OpenFileDialog();
+            this.buttonLoadSolution = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // panel
+            // 
+            this.panel.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panel.Location = new System.Drawing.Point(0, 41);
+            this.panel.Name = "panel";
+            this.panel.Size = new System.Drawing.Size(633, 586);
+            this.panel.TabIndex = 0;
+            this.panel.Paint += new System.Windows.Forms.PaintEventHandler(this.PanelPaint);
+            // 
+            // buttonLoadInstance
+            // 
+            this.buttonLoadInstance.Location = new System.Drawing.Point(12, 12);
+            this.buttonLoadInstance.Name = "buttonLoadInstance";
+            this.buttonLoadInstance.Size = new System.Drawing.Size(109, 23);
+            this.buttonLoadInstance.TabIndex = 1;
+            this.buttonLoadInstance.Text = "Load Instance";
+            this.buttonLoadInstance.UseVisualStyleBackColor = true;
+            this.buttonLoadInstance.Click += new System.EventHandler(this.ButtonLoadInstanceClick);
+            // 
+            // openFileDialog
+            // 
+            this.openFileDialog.AddExtension = false;
+            this.openFileDialog.RestoreDirectory = true;
+            // 
+            // buttonLoadSolution
+            // 
+            this.buttonLoadSolution.Location = new System.Drawing.Point(127, 12);
+            this.buttonLoadSolution.Name = "buttonLoadSolution";
+            this.buttonLoadSolution.Size = new System.Drawing.Size(107, 23);
+            this.buttonLoadSolution.TabIndex = 2;
+            this.buttonLoadSolution.Text = "Load Solution";
+            this.buttonLoadSolution.UseVisualStyleBackColor = true;
+            this.buttonLoadSolution.Click += new System.EventHandler(this.ButtonLoadSolutionClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(633, 627);
+            this.Controls.Add(this.buttonLoadSolution);
+            this.Controls.Add(this.buttonLoadInstance);
+            this.Controls.Add(this.panel);
+            this.Name = "Form1";
+            this.Text = "Visualizer";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel;
+        private System.Windows.Forms.Button buttonLoadInstance;
+        private System.Windows.Forms.OpenFileDialog openFileDialog;
+        private System.Windows.Forms.Button buttonLoadSolution;
+
+    }
+}
+
---
+++ Scm.Visualizer/Form1.cs
@@ -0,0 +1,114 @@
+﻿using Scm.Common;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Scm.Visualizer
+{
+    public partial class Form1 : Form
+    {
+        private Instance _instance;
+        private InstanceSolution _instanceSolution;
+        private int _colorIndex = 0;
+        private Color[] _colors = new[] { Color.Green, Color.Blue, Color.Orange, Color.Brown, Color.Violet, Color.Yellow, Color.Lime, Color.Magenta, Color.Indigo };
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private Point GetScaledPoint(Point max, Point current)
+        {
+            var gSize = new Point(panel.Size);
+            var xScaleFactor = (double)gSize.X / max.X;
+            var yScaleFactor = (double)gSize.Y / max.Y;
+
+            return new Point((int)(current.X * xScaleFactor), (int)(current.Y * yScaleFactor));
+        }
+
+        private void PanelPaint(object sender, PaintEventArgs e)
+        {
+            if (_instance != null)
+            {
+
+                var maxX = Math.Max(_instance.DepotList.Max(i => i.Point.X), _instance.CustomerList.Max(i => i.Point.X)) + 5;
+                var maxY = Math.Max(_instance.DepotList.Max(i => i.Point.Y), _instance.CustomerList.Max(i => i.Point.Y)) + 5;
+
+                foreach (var depot in _instance.DepotList)
+                {
+                    var point = GetScaledPoint(new Point(maxX, maxY), depot.Point);
+                    e.Graphics.FillEllipse(Brushes.Red, new RectangleF(point, new SizeF(10, 10)));
+                }
+
+                foreach (var customer in _instance.CustomerList)
+                {
+                    var point = GetScaledPoint(new Point(maxX, maxY), customer.Point);
+                    e.Graphics.FillEllipse(Brushes.Black, new RectangleF(point, new SizeF(10, 10)));
+                }
+            }
+
+            if (_instance != null && _instanceSolution != null)
+            {
+                var maxX = Math.Max(_instance.DepotList.Max(i => i.Point.X), _instance.CustomerList.Max(i => i.Point.X)) + 5;
+                var maxY = Math.Max(_instance.DepotList.Max(i => i.Point.Y), _instance.CustomerList.Max(i => i.Point.Y)) + 5;
+
+                foreach (var trail in _instanceSolution.Trails)
+                {
+                    for (var i=0; i<trail.Route.Count; i++)
+                    {
+                        if (i + 1 >= trail.Route.Count)
+                            break;
+
+                        var from = _instance.CustomerList.Where(l => l.Name == trail.Route[i]).Select(l => l.Point).SingleOrDefault();
+                        if (from == default(Point))
+                            from = _instance.DepotList[int.Parse(trail.DepotName) - 1].Point;
+
+                        var to = _instance.CustomerList.Where(l => l.Name == trail.Route[i+1]).Select(l => l.Point).SingleOrDefault();
+                        if (to == default(Point))
+                            to = _instance.DepotList[int.Parse(trail.DepotName) - 1].Point;
+
+                        if (from == null || to == null)
+                            continue;
+
+                        var fromPoint = GetScaledPoint(new Point(maxX, maxY), from);
+                        var toPoint = GetScaledPoint(new Point(maxX, maxY), to);
+
+                        e.Graphics.DrawLine(new Pen(_colors[_colorIndex % _colors.Length], 2f), fromPoint.X + 5, fromPoint.Y + 5, toPoint.X + 5, toPoint.Y + 5);
+                    }
+
+                    _colorIndex++;
+                }
+            }
+        }
+
+        private void ButtonLoadInstanceClick(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            _instanceSolution = null;
+            _instance = InstanceHelper.LoadInstance(openFileDialog.FileName);
+            panel.Refresh();
+        }
+
+        private void ButtonLoadSolutionClick(object sender, EventArgs e)
+        {
+            if (openFileDialog.ShowDialog() != System.Windows.Forms.DialogResult.OK)
+                return;
+
+            _instanceSolution = InstanceHelper.LoadInstanceSolution(openFileDialog.FileName);
+            panel.Refresh();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            panel.Invalidate();
+        }
+    }
+}
